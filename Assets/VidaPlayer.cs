28bdd97f616772e655dbbe8fba168f3630a60c3e
Gustavuoliveira <gustavo.dellanhol@gmail.com>
2017-06-21 15:40:09 -0300

    Inteligencia artificial
    IA - Anda por pontos pré-setados no mapa(precisa setar os pontos ainda) quando o player entra no campo de visão ela persegue e se encosta ataca(ataque aparentemente não esta funcionando direito), se perder o player de vista volta a procurar pelos pontos setados.
    
    Terreno - todo o terreno foi adaptado para que a IA possa definir o caminho automaticamente.

---
+++ Assets/VidaPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+public class LifePlayer : MonoBehaviour {
+	
+	public static float life = 100;
+	void Update (){
+		if (life <= 0) {
+			Debug.Log ("morreu");
+		}
+	}
+}
---
+++ Assets/_Scripts/Flashlight.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+	public Light LuzLanterna;
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			LuzLanterna.enabled = !LuzLanterna.enabled;﻿
+		}
+	}
+}
